1b764ff924fc388f85b40044bab24e797f2e8532
Charles Ji <me@charlesji.com>
2025-01-31 21:43:12 +0800

    remove redundant copilot stuff

--- lua/cheng/plugins/avente.lua
+++ lua/cheng/plugins/avente.lua
@@ -4,16 +4,23 @@ return {
     lazy = true,
     opts = {
         provider = "copilot",
-        auto_suggestions_provider = "copilot",
+        -- auto_suggestions_provider = "copilot",
         copilot = {
-            endpoint = "https://api.githubcopilot.com/",
-            -- model = "o1",
+            endpoint = "https://api.githubcopilot.com",
             model = "claude-3.5-sonnet",
             proxy = nil, -- [protocol://]host[:port] Use this proxy
             allow_insecure = false, -- Allow insecure server connections
             timeout = 30000, -- Timeout in milliseconds
             temperature = 0,
             max_tokens = 8192,
+        },
+        behaviour = {
+            auto_suggestions = false, -- Experimental stage
+            auto_set_highlight_group = true,
+            auto_set_keymaps = true,
+            auto_apply_diff_after_generation = false,
+            support_paste_from_clipboard = false,
+            minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
         },
     },
     build = "make",
