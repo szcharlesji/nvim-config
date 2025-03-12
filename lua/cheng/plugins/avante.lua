3fa0cddebcdb08aea345114bfb3e5ff0585432d4
Charles Ji <me@charlesji.com>
2025-03-12 14:16:44 -0400

    fix claude and gemini

--- lua/cheng/plugins/avante.lua
+++ lua/cheng/plugins/avante.lua
@@ -10,30 +10,27 @@ return {
             -- model = "claude-3.5-sonnet",
         },
         vendors = {
-            -- Available
             copilot_claude = {
                 __inherited_from = "copilot",
                 model = "claude-3.7-sonnet",
             },
-            -- Unavailable
             copilot_claude_thinking = {
                 __inherited_from = "copilot",
                 model = "claude-3.7-sonnet-thought",
+                temperature = 1,
+                max_tokens = 20000,
             },
-            -- Available
             copilot_o1 = {
                 __inherited_from = "copilot",
                 model = "o1",
             },
-            -- Available
             copilot_o3_mini = {
                 __inherited_from = "copilot",
                 model = "o3-mini",
             },
-            -- Unavailable
             copilot_gemini = {
                 __inherited_from = "copilot",
-                model = "gemini-2.0-flash-100",
+                model = "gemini-2.0-flash-001",
             },
         },
         behaviour = {
