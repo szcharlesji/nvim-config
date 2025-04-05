d6a40cbaa627352832f3947e4249925114090dc2
Charles Cheng Ji <me@charlesji.com>
2025-04-04 22:31:36 -0400

    remove unnecessary plugins

--- lua/cheng/plugins/snacks.lua
+++ lua/cheng/plugins/snacks.lua
@@ -4,8 +4,9 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
+    animate = { enabled = false },
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = { enabled = false },
     indent = { enabled = true },
     input = { enabled = false },
     notifier = { enabled = false },
@@ -81,24 +82,6 @@ return {
       end,
       desc = "Prev Reference",
       mode = { "n", "t" },
-    },
-    {
-      "<leader>N",
-      desc = "Neovim News",
-      function()
-        Snacks.win({
-          file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-          width = 0.6,
-          height = 0.6,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = "yes",
-            statuscolumn = " ",
-            conceallevel = 3,
-          },
-        })
-      end,
     },
   },
   init = function()
